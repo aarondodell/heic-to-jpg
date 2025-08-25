43b0522179a29cff13b9190cf555b28d228ec199
Aaron Dodell <aarondodell1@gmail.com>
2025-08-24 19:54:28 -0700

    recycle bin MVP working

--- FileUtils.py
+++ FileUtils.py
@@ -4,24 +4,28 @@ from PIL import Image
 
 register_heif_opener()
 
+RECYCLE_BIN_DIRNAME = 'HEIC_files_recycle_bin'
+
+
 def save_heic_to_jpg(heic_file_path):
-    image = Image.open(heic_file_path)    
+    image = Image.open(heic_file_path)
     file_root, ext = os.path.splitext(heic_file_path)
     jpg_file_path = file_root + '.JPG'
     image.save(jpg_file_path, "jpeg")
+    print (f'.JPG file created: {jpg_file_path}')
 
     return jpg_file_path
 
 def walkthrough_directory(dir_path, heic_files_found = []):
+    # TODO: skip recycle_bin paths in here somewhere
     for root, dirs, files in os.walk(dir_path):
         for file in files:
             if file.upper().endswith(".HEIC"):
                 full_file_path = os.path.join(root, file)
-                jpg_file_created = save_heic_to_jpg(full_file_path)
-
                 heic_files_found.append(full_file_path)
-                print (f'.HEIC file found: {full_file_path} | .jpg file created: {jpg_file_created}')
+                print (f'.HEIC file found: {full_file_path}')
         for dir in dirs:
-            walkthrough_directory(dir)
+            if os.path.basename(dir) != (RECYCLE_BIN_DIRNAME):
+                walkthrough_directory(dir)
 
-        
+    return heic_files_found
--- heic_to_jpg.py
+++ heic_to_jpg.py
@@ -1,9 +1,31 @@
 from FileUtils import *
 import os
+import shutil
 
 # main script
 PARENT_DIR = input("Provide dir to scan: ")
+RECYCLE_BIN_PATH = os.path.join(PARENT_DIR, 'HEIC_files_recycle_bin')
+
 if not os.path.exists(PARENT_DIR):
     raise ValueError(PARENT_DIR, "not a legitimate path")
 
-walkthrough_directory(PARENT_DIR)
+# find all files
+heic_files_found = walkthrough_directory(PARENT_DIR)
+
+# ask to create copies
+should_create_copies = input(f'Found {len(heic_files_found)} .HEIC files. Create copies at each location? (y/n)')
+if (should_create_copies == 'y'):
+    for heic_file in heic_files_found:
+        save_heic_to_jpg(heic_file)
+else:
+    exit()
+
+# ask to move HEIC files to a new recycle bin
+should_move_to_recycle_bin = input(f'Move the {len(heic_files_found)} .HEIC files to a new Recycle Bin folder? (y/n)')
+if (should_move_to_recycle_bin == 'y'):
+    if (not os.path.exists(RECYCLE_BIN_PATH)):
+        os.makedirs(RECYCLE_BIN_PATH)
+    for source_heic_file_path in heic_files_found:
+        destination_heic_file_path = os.path.join(RECYCLE_BIN_PATH, os.path.basename(source_heic_file_path))
+        shutil.move(source_heic_file_path, destination_heic_file_path)
+    print(f'Moved {len(heic_files_found)} into `{RECYCLE_BIN_PATH}`')
